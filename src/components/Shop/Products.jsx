d8cdc111ec8f7fc8b12136b8d76d21ad9c9190ee
Cory Pollard <astandard87@gmail.com>
2023-01-30 15:37:00 -0600

    Added functionality to add items to cart

--- src/components/Shop/Products.jsx
+++ src/components/Shop/Products.jsx
@@ -1,4 +1,5 @@
 import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart';
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 import DUMMY_ITEMS from '../../store/dummyItems';
@@ -7,7 +8,11 @@ const Products = props => {
   const dispatch = useDispatch();
 
   const addItemHandler = item => {
-    console.log(item);
+    const cartItem = {
+      ...item,
+      quantity: 1,
+    };
+    dispatch(cartActions.addItem(cartItem));
   };
 
   const items = DUMMY_ITEMS.map(item => {
@@ -26,15 +31,7 @@ const Products = props => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
-      <ul>
-        {/* <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product - amazing!"
-          onAddItem={addItemHandler.bind(null, item)}
-        /> */}
-        {items}
-      </ul>
+      <ul>{items}</ul>
     </section>
   );
 };
--- src/store/cart.jsx
+++ src/store/cart.jsx
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, current } from '@reduxjs/toolkit';
 
 const initialCartState = {
   cartOpen: false,
@@ -13,9 +13,42 @@ const cartSlice = createSlice({
     showCart(state) {
       state.cartOpen = !state.cartOpen;
     },
-    addItem(state, item) {
-      state.items = [...state.items, action.payload];
+
+    addItem(state, action) {
+      const existingItem = current(state.items).find(
+        item => item.id === action.payload.id
+      );
+
+      const existingItemIndex = current(state.items).findIndex(
+        item => item.id === action.payload.id
+      );
+
+      let item;
+
+      if (existingItem) {
+        const updatedQuantity = existingItem.quantity + 1;
+
+        item = {
+          ...existingItem,
+          quantity: updatedQuantity,
+          total: updatedQuantity * action.payload.price,
+        };
+        state.items[existingItemIndex] = item;
+      } else {
+        item = {
+          id: action.payload.id,
+          title: action.payload.title,
+          price: action.payload.price,
+          quantity: action.payload.quantity,
+          total: action.payload.price * action.payload.quantity,
+        };
+        state.items = [...state.items, item];
+      }
+
+      state.totalAmount = state.totalAmount + action.payload.quantity;
     },
+
+    removeItem(state, action) {},
   },
 });
 
