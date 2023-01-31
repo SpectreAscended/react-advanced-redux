92b5fe622a3aee710bf499f8e05733be5ea95ac9
Cory Pollard <astandard87@gmail.com>
2023-01-31 10:54:19 -0600

    added http request to attach cart to firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -5,6 +6,17 @@ import Products from './components/Shop/Products';
 
 function App() {
   const cartOpen = useSelector(state => state.ui.cartOpen);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-redux-f738e-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+  }, [cart]);
 
   return (
     <Layout>
